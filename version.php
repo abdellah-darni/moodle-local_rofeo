6475322f714e0abfb103dd4f93783db620805a65
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-08-09 19:04:11 +0100

    Fix required Moodle version in plugin metadata

--- version.php
+++ version.php
@@ -3,6 +3,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_rofeo';
 $plugin->version   = 2026080900;
-$plugin->requires  = 2026060800;
+$plugin->requires  = 2026042001;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1.0';
