6bce85106a6705065817b19a4a9dcdb2f2ae6114
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-08-20 13:07:28 +0100

    Add Open Graph meta tags for course detail page

--- course.php
+++ course.php
@@ -22,6 +22,44 @@ $PAGE->add_body_class('rofeo-has-footer');
 
 $detail = new \local_rofeo\output\course_detail($course);
 
+$ogtitle = format_string($course->fullname, true, ['context' => $context, 'escape' => false]);
+
+$ogdescription = format_text($course->summary, $course->summaryformat, ['context' => $context]);
+$ogdescription = strip_tags($ogdescription);
+$ogdescription = html_entity_decode($ogdescription, ENT_COMPAT);
+$ogdescription = trim($ogdescription);
+$ogdescription = shorten_text($ogdescription, 200);
+
+$ogimageurl = null;
+$listelement = new core_course_list_element($course);
+foreach ($listelement->get_course_overviewfiles() as $file) {
+    if ($file->is_valid_image()) {
+        $ogimageurl = moodle_url::make_file_url(
+            $CFG->wwwroot . '/pluginfile.php',
+            '/' . $file->get_contextid() . '/' . $file->get_component() . '/'
+                . $file->get_filearea() . $file->get_filepath() . $file->get_filename()
+        );
+        break;
+    }
+}
+
+$ogtags = [
+    html_writer::empty_tag('meta', ['property' => 'og:type', 'content' => 'article']),
+    html_writer::empty_tag('meta', ['property' => 'og:title', 'content' => $ogtitle]),
+    html_writer::empty_tag('meta', ['property' => 'og:description', 'content' => $ogdescription]),
+    html_writer::empty_tag('meta', ['property' => 'og:url', 'content' => $PAGE->url]),
+    html_writer::empty_tag('meta', ['name' => 'twitter:card', 'content' => 'summary_large_image']),
+];
+
+if ($ogimageurl) {
+    $ogtags[] = html_writer::empty_tag('meta', ['property' => 'og:image', 'content' => $ogimageurl]);
+}
+
+if (!isset($CFG->additionalhtmlhead)) {
+    $CFG->additionalhtmlhead = '';
+}
+$CFG->additionalhtmlhead .= "\n" . implode("\n", $ogtags);
+
 echo $OUTPUT->header();
 echo $OUTPUT->render_from_template('local_rofeo/course_detail', $detail->export_for_template($OUTPUT));
 echo $OUTPUT->footer();
